2a99cd51558f6aa0be8dffe35be5af2e0393f407
Marcelo Gonçalves <gbarros.marcelo@gmail.com>
2024-05-29 10:26:34 -0300

    aplicando estilos com bootstrap

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,7 +1,10 @@
 <x-layout title="Nova Série">
     <form action="" method="post">
-        <label for="nome">Nome:</label>
-        <input name="nome" type="text" value="" id="nome">
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome:</label>
+            <input name="nome" type="text" value="" id="nome" class="form-control">
+        </div>
+        <button type="submit" name="adicionar" class="btn btn-primary">Adicionar</button>
     </form>
 
 </x-layout>
